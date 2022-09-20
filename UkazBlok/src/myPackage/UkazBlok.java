a394c0fc9dd045eb880ec1550dd1f4273bedf9ab
greyplant <greyplant @10.0.0.10>
2022-09-20 12:28:33 +0200

    second commit		`

---
+++ UkazBlok/src/myPackage/UkazBlok.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+public class UkazBlok {
+	public static void main(String[] args) {
+		
+		double i, j, d;
+		
+		i = 5;
+		j = 10;
+		
+		if(i != 0) {
+			System.out.println("Premenna i sa nerovna nule.");
+			d = j / i;
+			System.out.println("Podiel j / i sa rovna " + d);
+		}
+		
+	}
+}
